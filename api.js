f6ded1b8b42ba90eb2bbdd31410671e1e6b5e2de
VitorOliveiraFranco <oliveirafrancovitorf@gmail.com>
2024-04-18 15:58:26 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,138 @@
+//17/04 criar dois novos endpoints
+
+//requires
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+//imports
+const mysql_config = require('./imp/mysql_config');
+const functions = require('./imp/functions');
+
+//variáveis para disponibilidade e para versionamento
+const API_AVAILABILITY = true;
+const API_VERSION = '1.0.0';
+
+//iniciar servidor
+const app = express();
+
+app.listen(3000, () => {
+
+    console.log('API está executando');
+});
+
+//verificar a disponibilidade da API
+app.use((req, res, next) => {
+
+    if(API_AVAILABILITY){
+
+        next();
+    } else {
+
+        res.json(functions.response('atenção', 'API está em manutenção. Sorry!', 0, null));
+    }
+});
+
+//conexão com o mysql
+const connection = mysql.createConnection(mysql_config);
+
+//cors
+app.use(cors());
+
+//rotas
+//rota inicial(entrada)
+app.get('/', (req, res) => {
+
+    res.json(functions.response('sucesso', 'API está rodando', 0, null));
+});
+
+//endpoint
+//rota pra a consulta completa
+app.get('/tasks', (req, res) => {
+
+    connection.query('SELECT * FROM tasks', (err, rows) => {
+        
+        if(!err){
+
+            res.json(functions.response('Sucesso', 'Sucesso na consulta', rows.length, rows));
+        } else {
+
+            res.json(functions.response('erro', err.message, 0, null));
+        }
+    });
+});
+
+//rota para fazer uma consulta da task por id
+app.get('/tasks/:id', (req, res) => {
+
+    const id = req.params.id;
+    connection.query('SELECT * FROM tasks WHERE id = ?', [id], (err, rows) => {
+
+        if(!err){
+
+            if(rows.length > 0){
+
+                res.json(functions.response('Sucesso', 'Sucesso na pesquisa', rows.length, rows));
+            } else {
+
+                res.json(functions.response('Atenção', 'Não foi encontrado a task selecionada', 0, null));
+            }
+        } else {
+
+            res.json(functions.response('erro', err.message, 0, null));
+        }
+    });
+});
+
+//rota para atualizar o status da task pelo id selecionado
+//app.put
+app.put('/tasks/:id/status/:status', (req, res) => {
+
+    const id = req.params.id;
+    const status = req.params.status;
+    connection.query('UPDATE tasks SET status = ? WHERE id = ?', [status, id], (err, rows) => {
+
+        if(!err){
+
+            if(rows.affectedRows > 0){
+
+                res.json(functions.response('Sucesso', 'Sucesso na alteração do status', rows.affectedRows, null));
+            } else {
+
+                res.json(functions.response('Alerta vermelho', 'Task não encontrada', 0, null));
+            }
+        } else {
+
+            res.json(functions.response('Erro', err.message, 0, null));
+        }
+    });
+});
+
+//rota para excluir uma task
+//método delete
+app.delete('/tasks/:id/delete', (req, res) => {
+
+    const id = req.params.id;
+    connection.query('DELETE FROM tasks WHERE id = ?', [id], (err, rows) => {
+
+        if(!err){
+
+            if(rows.affectedRows > 0){
+
+                res.json(functions.response('Sucesso', 'Task deletada', rows.affectedRows, null));
+            } else {
+
+                res.json(functions.response('Atenção', 'Task não encontrada', 0, null));
+            }
+        } else {
+
+            res.json(functions.response('Erro', err.message, 0, null));
+        }
+    });
+});
+
+//tratar o erro de rota
+app.use((req, res) => {
+
+    res.json(functions.response('atenção', 'rota não encontrada', 0, null));
+});
